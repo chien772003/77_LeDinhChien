7a7fbceb635564185acdafaa716230b09c1b52d9
chien772003 <101849850+chien772003@users.noreply.github.com>
2024-03-15 16:32:58 +0700

    Revert "Test"

--- 77_LeDinhChien.cpp
+++ 77_LeDinhChien.cpp
@@ -4,13 +4,8 @@ int main(){
 
 	cout << "77 Le Dinh Chien";
 	cout << endl;
-<<<<<<< HEAD
-	cout << "them 1 dong tu nhanh main" << endl;
-=======
 	cout << "test thu sua doi trên nhánh moi";
-cout << "them 1 dong o nhanh 77_LeDinhChien"
-
-cout << "test update";
+	cout<<"them 1 dong o nhanh 77_LeDinhChien"
 	system("pause");
 	return 0;
 }
